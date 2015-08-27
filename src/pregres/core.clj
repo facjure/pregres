4c7640660addd49f4d15797ed5a5230483622311
Priyatam Mudivarti <priyatam@gmail.com>
2015-08-27 11:44:06 -0700

    gen lein template

---
+++ src/pregres/core.clj
@@ -0,0 +1,6 @@
+(ns pregres.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/pregres/core_test.clj
@@ -0,0 +1,7 @@
+(ns pregres.core-test
+  (:require [clojure.test :refer :all]
+            [pregres.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
