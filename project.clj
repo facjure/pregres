19f112555c199326ca1a905c311eace28efbbafc
Priyatam Mudivarti <priyatam@gmail.com>
2015-08-27 19:04:40 -0700

    add basic deps

--- project.clj
+++ project.clj
@@ -1,6 +1,20 @@
 (defproject pregres "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :description "An integrated Clojure lib for Postgres"
+  :url "http://facjure.com/pregres"
+  :license {:name "Eclipse Public License" :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:name "git" :url "https://github.com/facjure/pregres"}
+  :min-lein-version "2.5.0"
+  :jvm-opts ["-Xmx1g"]
+  :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [org.clojure/data.csv "0.1.3"]
+                 [org.clojure/data.json "0.2.6"]
+                 [org.postgresql/postgresql "9.4-1201-jdbc41"]
+                 [com.cognitect/transit-clj "0.8.275"]
+                 [prismatic/schema "0.4.3"]
+                 [ch.qos.logback/logback-classic "1.0.1"]
+                 [yesql "0.4.2"]
+                 [ragtime "0.5.1"]
+                 [clj-time "0.10.0"]
+                 [environ "1.0.0"]])
