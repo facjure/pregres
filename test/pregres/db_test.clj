ae5579fcfbdcff48bd507083c7054775e2bcd603
Priyatam Mudivarti <priyatam@gmail.com>
2015-09-20 20:44:28 -0700

    add db test
    - tests pass

--- test/pregres/db_test.clj
+++ test/pregres/db_test.clj
@@ -1,7 +1,22 @@
 (ns pregres.db-test
   (:require [clojure.test :refer :all]
+            [pregres.core.test-fixtures :as fixtures]
             [pregres.db :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(use-fixtures :once fixtures/db)
+
+(def bad-db-config {})
+(def good-db-config {:database-url "postgresql://localhost:5432/"})
+
+(deftest connections
+  (testing "Connect without any params?"
+    (is (contains? (connect)  :datasource)))
+  (testing "Connect with bad db config?"
+    (is (connect good-db-config) :datasource))
+  (testing "Connect with good db config and find info"
+    (let [db (connect good-db-config)]
+      (contains? db :datasource)
+      (contains? (info db) :schemas)))
+  (testing "Disconnect bad conn"
+    (let [db (connect good-db-config)]
+      (disconnect db))))
