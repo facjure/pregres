06c05b172a7043e141501008a5a5bba2a3defd0d
Priyatam Mudivarti <priyatam@gmail.com>
2015-09-20 20:45:51 -0700

    simplify queries (wip)

--- src/pregres/queries.clj
+++ src/pregres/queries.clj
@@ -3,15 +3,28 @@
   (:require
    [clojure.java.jdbc :as jdbc]
    [clojure.string :as str]
+   [clojure.java.io :as io]
    [hikari-cp.core :as hikari]
    [environ.core :refer :all]
-   [pregres.utils :as utils])
+   [pregres.db :as db]
+   [pregres.core.utils :as utils])
   (:import
    [java.sql Timestamp]
    [java.util Date UUID]
    [org.postgresql.jdbc4 Jdbc4Array]
    [org.postgresql.util PGobject]
    [java.net.URI]))
+
+;; -------
+;; Helpers
+
+(defn- row-transform [only-id? row]
+  (if only-id?
+    (:id row)
+    row))
+
+(defn current-sql-time []
+  (Timestamp. (utils/now)))
 
 (defn in [xs]
   (format "IN (%s)"
@@ -25,56 +38,58 @@
                (repeat (count xs))
                (str/join ", "))))
 
-(defn read
-  "Read sequences of 0 or more rows"
-  [db-spec sql-params]
-  (jdbc/query db-spec sql-params
-              :identifiers utils/underscores->hyphens))
+;; ---
+;; API
 
-(defn find
-  "Pull 1 or nil rows from the DB"
-  [db-spec sql-args]
-  (first (read db-spec sql-args)))
-
-(defn- row-transform [only-id? row]
-  (if only-id?
-    (:id row)
-    row))
-
-(defn create [db-spec table m & {:keys [only-id?] :or {only-id? true}}]
-  (->> (jdbc/insert! db-spec table m :entities utils/keyword->underscored-string)
+(defn create [db table m & {:keys [only-id?] :or {only-id? true}}]
+  (->> (jdbc/insert! db table m :entities utils/keyword->underscored-string)
        utils/only
        (row-transform only-id?)))
 
-(defn create-batch [db-spec table ms & {:keys [only-id?] :or {only-id? true}}]
+(defn create-batch [db table ms & {:keys [only-id?] :or {only-id? true}}]
   (->> (apply jdbc/insert!
-              db-spec
+              db
               table
               (conj (vec ms) :entities utils/keyword->underscored-string))
        (map #(row-transform only-id? %))))
 
-(defn current-sql-time []
-  (Timestamp. (utils/now)))
+(defn read
+  "Read sequences of 0 or more rows"
+  [db sql-params]
+  (jdbc/query db sql-params
+              :identifiers utils/underscores->hyphens))
 
 (defn update
   "Update 0-many rows in the DB"
-  [db-spec table m where-clause]
-  (jdbc/update! db-spec
+  [db table m where-clause]
+  (jdbc/update! db
                 table
                 (assoc m :updated-at (current-sql-time))
                 where-clause
                 :entities utils/keyword->underscored-string)
   nil)
 
+(defn delete [db table where-clause]
+  (jdbc/delete! db table where-clause
+                :entities utils/keyword->underscored-string))
+
 (defn soft-delete
   "Mark row as (soft) deleted, but keep it in the DB."
-  [db-spec table id]
-  (update db-spec
+  [db table id]
+  (update db
           table
           {:deleted-at (current-sql-time)}
           ["id = ?" id])
   nil)
 
-(defn delete [db-spec table where-clause]
-  (jdbc/delete! db-spec table where-clause
-                :entities utils/keyword->underscored-string))
+(defn find
+  "Pull 1 or nil rows from the DB"
+  [db sql-args]
+  (first (read db sql-args)))
+
+(defn run-script
+  "Executes a SQL script under resources dir."
+  [ds sql-file]
+  (let [commands (slurp (io/resource sql-file))]
+    (jdbc/db-do-commands ds commands)))
+
