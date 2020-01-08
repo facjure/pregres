f3f8e75873db2455953ab7727f6564537e745254
Priyatam Mudivarti <priyatam@facjure.com>
2020-01-07 20:30:43 -0800

    update libs, cleanup deps

--- project.clj
+++ project.clj
@@ -1,21 +1,17 @@
-(defproject facjure/pregres "0.2.0-SNAPSHOT"
-  :description "A simple interface to Postgres"
+(defproject facjure/pregres "0.3.0-SNAPSHOT"
+ :description "A Clojure interface to Postgres"
   :url "http://facjure.com/pregres"
   :license {:name "Eclipse Public License" :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git" :url "https://github.com/facjure/pregres"}
-  :min-lein-version "2.5.0"
+  :min-lein-version "2.8.0"
   :global-vars {*warn-on-reflection* false *assert* false}
   :jvm-opts ["-Xmx512m"]
-  :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
-                 [org.clojure/data.csv "0.1.3"]
-                 [org.clojure/java.jdbc "0.4.2"]
-                 [org.clojure/tools.logging "0.3.1"]
-                 [org.postgresql/postgresql "9.4-1206-jdbc41"]
-                 [com.h2database/h2 "1.4.191"]
-                 [com.stuartsierra/component "0.3.1"]
-                 [prismatic/schema "1.0.1"]
-                 [cheshire "5.5.0"]
-                 [clj-time "0.11.0"]
-                 [hikari-cp "1.3.1"]
-                 [ragtime "0.5.2"]
-                 [yesql "0.5.2"]])
+  :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
+                 [org.clojure/data.csv "0.1.4"]
+                 [org.clojure/java.jdbc "0.7.11"]
+                 [org.clojure/tools.logging "0.5.0"]
+                 [org.postgresql/postgresql "42.2.9"]
+                 [clojure.java-time "0.3.2"]
+                 [cheshire "5.9.0"]
+                 [hikari-cp "2.10.0"]
+                 [ragtime "0.8.0"]])
